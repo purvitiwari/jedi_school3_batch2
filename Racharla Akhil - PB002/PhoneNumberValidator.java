61e04cb76150b304be6a3b9157e73d6b976148e1
Akhil Racharla <racharlaakhil@practo.com>
2016-08-02 20:39:38 +0530

    added README.md

--- Racharla Akhil - PB002/PhoneNumberValidator.java
+++ Racharla Akhil - PB002/PhoneNumberValidator.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class PhoneNumberValidator {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		while(true) {
+		String phoneNumber;
+		if(args.length > 0) {
+			phoneNumber = args[0];
+		} else {
+			Scanner input = new Scanner(System.in);
 			System.out.println("Enter Phone number");
-			String phoneNumber = input.nextLine();
-			String regex = "^((0)|(\\+91))?([7,8,9])(\\d{9})";
-			System.out.println(phoneNumber +  " is " + (phoneNumber.matches(regex) ? ("valid") : "invalid") + " phone number.");
+			phoneNumber = input.nextLine();
+			input.close();
 		}
+		
+		String regex = "^((0)|(\\+91))?([7,8,9])(\\d{9})";
+		System.out.println(phoneNumber +  " is " + (phoneNumber.matches(regex) ? ("valid") : "invalid") + " phone number.");
 	}
 }
